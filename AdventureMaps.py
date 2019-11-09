984fc675c0e1e9d301ae78e5adff9f6c60bbaa61
OneBite <35378466+OneBiteAidan@users.noreply.github.com>
2019-11-09 16:08:06 -0500

    Finished the next_destination function
    Might need some spacing at a later time.

--- AdventureMaps.py
+++ AdventureMaps.py
@@ -34,11 +34,55 @@ def next_destination():
     if input == 1:
         print_ragni()
         ready = input("Ready to go to Ragni? Press ENTER when ready.")
-        if input = (""):
+        if input == (""):
             townsquare.ragni_square()
         else:
             townsquare.ragni_square()
-
+    elif input == 2:
+        print_detlas()
+        ready = input("Ready to go to Detlas? Press ENTER when ready.")
+        if input == (""):
+            townsquare.detlas_square()
+        else:
+            townsquare.detlas_square()
+    elif input == 3:
+        print_almuj()
+        ready = input("Ready to go to Almuj? Press ENTER when ready.")
+        if input == (""):
+            townsquare.almuj_square()
+        else:
+            townsquare.almuj_square()
+    elif input == 4:
+        print_nemract()
+        ready = input("Ready to go to Nemract? Press ENTER when ready.")
+        if input == (""):
+            townsquare.nemract_square()
+        else:
+            townsquare.nemract_square()
+    elif input == 5:
+        print_nesaak()
+        ready = input("Ready to go to Nesaak? Press ENTER when ready.")
+        if input == (""):
+            townsquare.nesaak_square()
+        else:
+            townsquare.nesaak_square()
+    elif input == 6:
+        print_troms()
+        ready = input("Ready to go to Troms? Press ENTER when ready.")
+        if input == (""):
+            townsquare.troms_square()
+        else:
+            townsquare.troms_square()
+    elif input == 7:
+        print_rymek()
+        ready = input("Ready to go to Rymek? Press ENTER when ready.")
+        if input == (""):
+            townsquare.rymek_sqaure()
+        else:
+            townsquare.rymek_sqaure()
+    else:
+        print ("That is not a valid option.")
+        next_destination()
 
 def print_ragni():
     print ("")
